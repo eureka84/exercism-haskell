5c447ff4b6e45f7f66d41f984ac6ee4908e85b91
eureka84 <angelosciarra@ymail.com>
2018-11-27 00:27:51 +0100

    [WIP] acronym made loop an inner function of splitCamelCase

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -2,14 +2,12 @@ module Acronym (abbreviate) where
 
 import Data.Char
 
-loop :: String -> String -> [String] -> [String]
-loop [] curr res                  = res++[curr]
-loop (x:xs) curr res | isUpper x  = case curr of [] -> loop xs [x] res
-                                                 s  -> loop xs [x] (res++[s])
-loop (x:xs) curr res              = loop xs (curr++[x]) res
-
 splitCamelCase :: String -> [String]
 splitCamelCase word = loop word [] []
+  where loop [] curr res                  = res++[curr]
+        loop (x:xs) curr res | isUpper x  = case curr of [] -> loop xs [x] res
+                                                         s  -> loop xs [x] (res++[s])
+        loop (x:xs) curr res              = loop xs (curr++[x]) res
 
 split :: String -> [String]
 split input = concat (splitCamelCase `map` (words input))
