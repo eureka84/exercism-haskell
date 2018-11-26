b98514011053618ac4b431cea9ad410c26bdaed3
eureka84 <angelosciarra@ymail.com>
2018-11-26 17:18:48 +0100

    used guards to implement leap

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -4,5 +4,7 @@ isDivisibleBy :: Integer -> Integer -> Bool
 isDivisibleBy number divisor = (number `mod` divisor) == 0
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = (year `isDivisibleBy` 4 && not(year `isDivisibleBy` 100))
-                || year `isDivisibleBy` 400
+isLeapYear year
+  | year `isDivisibleBy` 4 && not(year `isDivisibleBy` 100) = True
+  | year `isDivisibleBy` 400                                = True
+  | otherwise                                               = False
