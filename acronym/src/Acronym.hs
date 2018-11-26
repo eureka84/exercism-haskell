87af1766e96fcc18ed9bd9091378bee0dea683fc
eureka84 <angelosciarra@ymail.com>
2018-11-27 00:18:04 +0100

    [WIP] - acronym

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -2,6 +2,20 @@ module Acronym (abbreviate) where
 
 import Data.Char
 
+loop :: String -> String -> [String] -> [String]
+loop [] curr res                  = res++[curr]
+loop (x:xs) curr res | isUpper x  = case curr of [] -> loop xs [x] res
+                                                 s  -> loop xs [x] (res++[s])
+loop (x:xs) curr res              = loop xs (curr++[x]) res
+
+splitCamelCase :: String -> [String]
+splitCamelCase word = loop word [] []
+
+split :: String -> [String]
+split input = concat (splitCamelCase `map` (words input))
+
+firstLetterCapitalized :: String -> Char
+firstLetterCapitalized word = toUpper(head word)
+
 abbreviate :: String -> String
-abbreviate xs =
-   (\x -> toUpper(head x)) `map` (words xs)
+abbreviate input = firstLetterCapitalized `map` (split input)
