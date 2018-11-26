3c28181b06a6171f8d9de32c3b2ceb0540dfc909
eureka84 <angelosciarra@ymail.com>
2018-11-26 17:12:43 +0100

    tidy up leap

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -3,15 +3,6 @@ module LeapYear (isLeapYear) where
 isDivisibleBy :: Integer -> Integer -> Bool
 isDivisibleBy number divisor = (number `mod` divisor) == 0
 
-isDivisibleBy400 :: Integer -> Bool
-isDivisibleBy400 number = number `isDivisibleBy` 400
-
-isDivisibleBy100 :: Integer -> Bool
-isDivisibleBy100 number = number `isDivisibleBy` 100
-
-isDivisibleBy4 :: Integer -> Bool
-isDivisibleBy4 number = number `isDivisibleBy` 4
-
 isLeapYear :: Integer -> Bool
-isLeapYear year = (isDivisibleBy400 year)
-               || (isDivisibleBy4 year && not(isDivisibleBy100 year))
+isLeapYear year = (year `isDivisibleBy` 4 && not(year `isDivisibleBy` 100))
+                || year `isDivisibleBy` 400
