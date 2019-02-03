a290cbd960c7c1d565ad21aee48c361622f13769
eureka84 <angelosciarra@ymail.com>
2019-02-03 19:37:58 +0100

    refactor acronym

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -5,19 +5,19 @@ import Data.List.Split
 
 splitCamelCase :: String -> [String]
 splitCamelCase word = loop word [] []
-  where loop remaining curr res = case remaining of
-          [] -> res++[curr]
-          (x:xs) | isUpper x -> case curr of
-                    [] -> loop xs [x] res
-                    s | isLower (last s) -> loop xs [x] (res++[s])
-                    _ -> loop xs (curr++[x]) res
-          (x:xs) -> loop xs (curr++[x]) res
+  where loop remaining curr result = case remaining of
+      []                           -> result++[curr]
+      (first:rest) | isUpper first -> case curr of
+                                        []                    -> loop rest [first] result
+                                        s | isLower (last s)  -> loop rest [first] (result++[s])
+                                        _                     -> loop rest (curr++[first]) result
+      (first:rest)                 -> loop rest (curr++[first]) result
 
 separateWords :: String -> [String]
-separateWords phrase = filter (\x ->  not(null x)) (splitOneOf " -," phrase)
+separateWords phrase = filter (not . null) (splitOneOf " -," phrase)
 
 splitInput :: String -> [String]
-splitInput input = concat (map splitCamelCase (separateWords input))
+splitInput input = concat $ map splitCamelCase $ separateWords input
 
 abbreviate :: String -> String
 abbreviate input = map (toUpper . head) (splitInput input)
