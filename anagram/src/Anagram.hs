38b99c8903ba88ad6a570e863329726ac6915f74
eureka84 <angelosciarra@ymail.com>
2019-02-03 23:20:53 +0100

    minor

--- anagram/src/Anagram.hs
+++ anagram/src/Anagram.hs
@@ -7,10 +7,10 @@ anagramsFor :: String -> [String] -> [String]
 anagramsFor xs xss = filter (\x -> x `isAnagramOf` xs) xss
 
 isAnagramOf :: String -> String -> Bool
-isAnagramOf candidate original = hasSameLetters && not(sameWord)
+isAnagramOf candidate original = hasSameLetters && not(isSameWord)
   where
     lOriginal = map toLower original
     lCandidate = map toLower candidate
     hasSameLetters = (sort lCandidate) == (sort lOriginal)
-    sameWord = lCandidate == lOriginal
+    isSameWord = lCandidate == lOriginal
 
