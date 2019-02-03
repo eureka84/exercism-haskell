a7184cd228159df24494b149b9fa6aadf255c5d7
eureka84 <angelosciarra@ymail.com>
2019-02-03 18:15:23 +0100

    [DONE] space age

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -9,22 +9,20 @@ data Planet = Mercury
             | Uranus
             | Neptune
 
-secondsToEarthYears :: Float -> Float
-secondsToEarthYears seconds = seconds / secondsInAYear
-  where
-      secondsInADay = 24 * 60 * 60
-      secondsInAYear = 365.25 * secondsInADay
-
-yearRatioToEarth :: Planet -> Float
-yearRatioToEarth planet =  case planet of
-   Mercury -> 0.2408467
-   Venus -> 0.61519726
-   Earth -> 1
-   Mars -> 1.8808158
-   Jupiter -> 11.862615
-   Saturn -> 29.4479498
-   Uranus -> 84.016846
-   Neptune -> 164.79132
-
 ageOn :: Planet -> Float -> Float
-ageOn planet = (/ yearRatioToEarth planet) . secondsToEarthYears
+ageOn planet seconds = (/ yearRatioToEarth planet) $ secondsToEarthYears seconds
+  where
+    yearRatioToEarth p = case p of
+       Mercury -> 0.2408467
+       Venus -> 0.61519726
+       Earth -> 1
+       Mars -> 1.8808158
+       Jupiter -> 11.862615
+       Saturn -> 29.4479498
+       Uranus -> 84.016846
+       Neptune -> 164.79132
+    secondsToEarthYears s =
+      let secondsInADay   = 24 * 60 * 60
+          secondsInAYear  = 365.25 * secondsInADay
+      in  s / secondsInAYear
+
