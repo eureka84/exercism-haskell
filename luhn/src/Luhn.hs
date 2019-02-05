1a3b33baba83402e02d7872dd9cec5b76237e6d5
eureka84 <angelosciarra@ymail.com>
2019-02-05 23:51:59 +0100

    [DONE] Luhn

--- luhn/src/Luhn.hs
+++ luhn/src/Luhn.hs
@@ -12,9 +12,9 @@ isValid' [] = False
 isValid' [_] = False
 isValid' xs = (checkSum `mod` 10) == 0
   where
-    checkSum = sum $ map (\(index, c) -> processDigit index c) $ indexed $ reverse xsDigits
+    checkSum = sum $ map (\(index, c) -> maybeDouble index c) $ indexed $ reverse xsDigits
     xsDigits = map digitToInt xs
-    processDigit index digit
+    maybeDouble index digit
       | index `mod` 2 == 0 = digit
       | otherwise = if (double > 9) then double - 9 else double
           where double = 2 * digit
