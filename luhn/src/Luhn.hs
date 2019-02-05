de8c1d7f5bb2d446f81d5e678e2c20da3546b77a
eureka84 <angelosciarra@ymail.com>
2019-02-05 23:45:59 +0100

    [DONE] Luhn

---
+++ luhn/src/Luhn.hs
@@ -0,0 +1,20 @@
+module Luhn (isValid) where
+
+import Data.Char
+import Data.List.Index
+
+isValid :: String -> Bool
+isValid xs = isValid' filtered
+  where filtered = filter (\x -> x /= ' ') xs
+
+isValid' :: String -> Bool
+isValid' [] = False
+isValid' [_] = False
+isValid' xs = (checkSum `mod` 10) == 0
+  where
+    checkSum = sum $ map (\(index, c) -> processDigit index c) $ indexed $ reverse xsDigits
+    xsDigits = map digitToInt xs
+    processDigit index digit
+      | index `mod` 2 == 0 = digit
+      | otherwise = if (double > 9) then double - 9 else double
+          where double = 2 * digit
