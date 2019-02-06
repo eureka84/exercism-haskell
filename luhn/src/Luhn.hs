8e118e49c3249a49ad0d5d1f5439ac731cd9ebe4
asciarra <angelo.sciarra@lastminute.com>
2019-02-06 17:33:35 +0100

    minor: reformat

--- luhn/src/Luhn.hs
+++ luhn/src/Luhn.hs
@@ -5,18 +5,20 @@ import Data.List.Index
 
 isValid :: String -> Bool
 isValid xs = isValid' filtered
-  where filtered = filter (\x -> x /= ' ') xs
+  where 
+    filtered = filter (\x -> x /= ' ') xs
 
 isValid' :: String -> Bool
-isValid' [] = False
+isValid' []  = False
 isValid' [_] = False
-isValid' xs = (checkSum `mod` 10) == 0
+isValid' xs  = (checkSum `mod` 10) == 0
   where
     checkSum = sum $ imap maybeDouble $ reverse xsDigits
     xsDigits = map digitToInt xs
     maybeDouble index digit
       | index `mod` 2 == 0 = digit
-      | otherwise = if (double > 9) then double - 9 else double
-          where double = 2 * digit
+      | otherwise          = if (double > 9) then double - 9 else double
+          where 
+            double = 2 * digit 
 
 
