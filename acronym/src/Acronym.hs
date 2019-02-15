ad3169da65c9077031d71a96bd7ead4c409c5841
eureka84 <angelosciarra@ymail.com>
2019-02-15 18:18:35 +0100

    refactor acronym - rewrote splitCamelCase

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -1,17 +1,18 @@
 module Acronym (abbreviate) where
 
 import Data.Char
+import Data.List
 import Data.List.Split
 
 splitCamelCase :: String -> [String]
-splitCamelCase word = loop word [] []
-  where loop remaining curr res = case remaining of
-          []                  -> res++[curr]
-          (x:xs) | isUpper x  -> case curr of
-                                    []                    -> loop xs [x] res
-                                    s | isLower (last s)  -> loop xs [x] (res++[s])
-                                    _                     -> loop xs (curr++[x]) res
-          (x:xs)              -> loop xs (curr++[x]) res
+splitCamelCase word
+ | all isLower word = [word]
+ | all isUpper word = [word]
+ | otherwise        = map (\(f, s) -> f ++ s) $
+                      filter (\(f,_) -> length f == 1) $
+                      zip (take (length grouped - 1) grouped) (drop 1 grouped)
+  where
+    grouped = groupBy (\x y -> isLower x && isLower y) $ filter isAlpha word
 
 separateWords :: String -> [String]
 separateWords = filter (not . null) .  splitOneOf " -,"
