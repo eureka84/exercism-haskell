3fefae8c18530417da2b29217e066da83a43f33b
eureka84 <angelosciarra@ymail.com>
2019-03-01 08:23:04 +0100

    [DONE] pangram

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -1,13 +1,7 @@
 module Pangram (isPangram) where
 
-import qualified Data.Set as Set
-import Data.Char
-import Data.List
+import Data.Char (isAlpha, toLower)
 
--- working for infinite strings
 isPangram :: String -> Bool
-isPangram = any ifPangram  . map toCharactersSet . inits
-  where
-    ifPangram xs = alphabet == xs
-    alphabet = Set.fromList ['a'..'z']
-    toCharactersSet = Set.fromList . map toLower . filter isAlpha
+isPangram text = all (`elem` letters) ['a' .. 'z']
+    where letters = map toLower $ filter isAlpha text
